a03c2dce35c5a1cac1eae0827c418956629fe921
helloa1109 <helloa1109@naver.com>
2025-08-30 17:42:52 +0900

    Updates

---
+++ _next/static/chunks/app/error-05a264343ee6c586.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{1390:function(e,r,t){Promise.resolve().then(t.bind(t,1598))},1598:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return Error}});var n=t(7437);function Error(e){let{error:r,reset:t}=e;return(0,n.jsxs)("div",{className:"flex flex-col items-center justify-center min-h-screen",children:[(0,n.jsx)("h2",{className:"text-2xl font-bold mb-4",children:"문제가 발생했습니다"}),(0,n.jsx)("button",{className:"px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600",onClick:()=>t(),children:"다시 시도"})]})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,l={},f=null,i=null;for(n in void 0!==t&&(f=""+t),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(i=r.ref),r)s.call(r,n)&&!u.hasOwnProperty(n)&&(l[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===l[n]&&(l[n]=r[n]);return{$$typeof:o,type:e,key:f,ref:i,props:l,_owner:c.current}}r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=1390)}),_N_E=e.O()}]);
