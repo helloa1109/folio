6e50f53dcf1e6ff5f483e550c3db81b9642344d8
helloa1109 <helloa1109@naver.com>
2025-08-30 23:38:31 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -2,17 +2,14 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  assetPrefix :
-    process.env.NODE_ENV === 'production'
-    ? 'https://helloa1109.github.io/folio'
-    : ''?
+  assetPrefix: isProd ? '/folio/' : '',
+  basePath: isProd ? '/folio' : '',
   output: 'export',
   reactStrictMode: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   trailingSlash: true,
 };
-
 
 module.exports = nextConfig;
