6dda594238d30de1d8ea9d429370296560d3244f
helloa1109 <helloa1109@naver.com>
2025-08-30 23:40:21 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd ? '/folio/' : '',
-  basePath: isProd ? '/folio' : '',
-  output: 'export',
+  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  // ✅ 중요한 부분
+  basePath: isProd ? "/folio" : "",
+  assetPrefix: isProd ? "/folio/" : "",
 };
 
 module.exports = nextConfig;
