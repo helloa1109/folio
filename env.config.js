04ff4f623cf4e0be15c4d0b518d6e347f6d368ba
helloa1109 <helloa1109@naver.com>
2025-08-31 12:57:11 +0900

    fix:config image code

--- env.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'process.env.BACKEND_URL':
-      process.env.NODE_ENV === 'production'
-        ? 'https://helloa1109.github.io/folio'
-        : '',
-  };
--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
