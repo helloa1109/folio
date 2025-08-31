355c81dfd3875291ebfa5f4ab1d271b138f36e82
helloa1109 <helloa1109@naver.com>
2025-08-31 12:34:55 +0900

    fix:nextconfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 // next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // ✅ next export 대체
-  images: {
-    unoptimized: true, // GitHub Pages에서는 이미지 최적화 안 됨
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    };
+    return config;
   },
-  basePath: "/folio", // ✅ 레포 이름 (깃허브 주소에 맞춰줘야 함)
-  assetPrefix: "/folio/", // ✅ CSS/JS 경로 맞추기
 };
 
 module.exports = nextConfig;
